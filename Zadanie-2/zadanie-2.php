e083e4983e47ea2784120bbeb395570f62fc27fe
karpovvicz <karpovvicz@gmail.com>
2025-04-08 18:17:08 +0200

    FIX zadanie-2.php
    Poprawienie błędu składniowego w zapisie reguły regex

--- Zadanie-2/zadanie-2.php
+++ Zadanie-2/zadanie-2.php
@@ -2,7 +2,7 @@
 
 function konwertujExcelNumerycznie(string $cellAddress): string {
   //Uwierzytelnienie danych wejściowych
-  if (!preg_match('/[A-Z]+)(\d)$/i', strtoupper($cellAddress), $matches)) {
+  if (!preg_match('/^([A-Z]+)(\d+)$/i', strtoupper($cellAddress), $matches)) {
     throw new InvalidArgumentException("Niepoprawny format komórki: $cellAddress");
   }
 
