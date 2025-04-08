da7d6cc340494ce982ec42bdc6549e593e6db16a
karpovvicz <karpovvicz@gmail.com>
2025-04-08 18:52:43 +0200

    ADD zadanie-3.php
    Dodanie konfiguracji bazy danych z podstawową konstrukcją try/catch informującą błędnym połączeniu

--- Zadanie-3/zadanie-3.php
+++ Zadanie-3/zadanie-3.php
@@ -1,1 +1,13 @@
+<?php 
+//Konfiguracja bazy danych 
+$dsn = 'mysql:host=localhost;dbname=test;charset=utf8mb4';
+$dbUser = 'root';
+$dbPass =";
 
+try {
+$pdo=new PDO($dsn, $dbUser, $dbPass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+} catch (PDOException $e) {
+exit("Błąd połączenia z bazą danych: " . $e->getMessage());
+
+}
