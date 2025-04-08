155884ecf249f3c4c350db1166545990bf03a75f
karpovvicz <karpovvicz@gmail.com>
2025-04-08 17:41:22 +0200

    ADD zadanie-2.php
    Uzupełnienie funkcji o konwersję liter kolumny na wartość numeryczną

--- Zadanie-2/zadanie-2.php
+++ Zadanie-2/zadanie-2.php
@@ -6,7 +6,16 @@ function konwertujExcelNumerycznie(string $cellAddress): string {
     throw new InvalidArgumentException("Niepoprawny format komórki: $cellAddress");
   }
 
+[$_, $columnLetters, $rowNumbers] = $matches;
 
+  $columnNumber = 0;
+  $length = strlen($columnLetters);
+  for ($i = 0; $i < $length; $i++) {
+    $char = $columnLetters[$i];
+    $columnNumber *= 26;
+    $columnNumber += ord($char) - ord('A') +1;
+  }
 
+  return $columnNumber . '.' . (int)$rowNumber;
   
 }
