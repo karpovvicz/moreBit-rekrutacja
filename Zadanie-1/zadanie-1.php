3d897145d886068dc07b863c67caa4ee69d82c99
karpovvicz <karpovvicz@gmail.com>
2025-04-08 10:15:23 +0200

    ADD zadanie-1.php
    dodanie głównej metody generującej kalendarz w html

--- Zadanie-1/zadanie-1.php
+++ Zadanie-1/zadanie-1.php
@@ -1,1 +1,22 @@
+<?php 
 
+*główna metoda generująca kalendarz w HTML
+
+class KalendarzGenerator 
+  {
+    private int $month;
+    private int $year;
+
+    public function __construct(int $month, int $year)
+    {
+      if ($month < 1 || $month > 12) {
+        trow new InvalidArgumentException("Podaj zakres miesięczny 1-12.");
+    
+    }
+
+      $this->month = $month;
+      $this->year = $year; 
+
+    }
+
+@return string 
