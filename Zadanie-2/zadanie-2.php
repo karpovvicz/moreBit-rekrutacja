002ffe14d73442e1c04f5b49c4ffac1dce36d64b
karpovvicz <karpovvicz@gmail.com>
2025-04-08 17:11:35 +0200

    ADD zadanie-2.php
    Dodanie podstawowej weryfikacji danych wejśćiowych

--- Zadanie-2/zadanie-2.php
+++ Zadanie-2/zadanie-2.php
@@ -1,1 +1,12 @@
+<?php 
 
+function konwertujExcelNumerycznie(string $cellAddress): string {
+  //Uwierzytelnienie danych wejściowych
+  if (!preg_match('/[A-Z]+)(\d)$/i', strtoupper($cellAddress), $matches)) {
+    throw new InvalidArgumentException("Niepoprawny format komórki: $cellAddress");
+  }
+
+
+
+  
+}
